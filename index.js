29c1cd2177f16d573165413806c38e6c0ad8ae6d
girelle Dongmo <dongmogirelle@gmail.com>
2026-09-09 19:46:56 -0700

    fix: correction des routes API et configuration Render

--- index.js
+++ index.js
@@ -44,37 +44,26 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
-   Note.findByIdAndDelete(request.params.id)
-   .then(result => {
-   response.status(204).end()
-   })
-   .catch(error => next(error))
-   })
-
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-  
 
 app.get('/api/notes/:id', (request, response, next) => {
-   Note.findById(request.params.id)
-   .then(note => {
-   if (note) {
-   response.json(note)
-   } else {
-   response.status(404).end()
-   }
-   })
-  .catch(error => next(error))
-   })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
   
   if (!body.content) {
@@ -83,49 +72,34 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  app.get('/api/notes/:id', (request, response) => {
-     Note.findById(request.params.id)
-     .then(note => {
-     if (note) {
-     response.json(note)
-     } else {
-     response.status(404).end()
-    }
-    
-    })
-     .catch(error => {
-     console.log(error)
-     response.status(400).send({ error: 'malformatted id' })
-     })
-    })
-
-    app.put('/api/notes/:id', (request, response, next) => {
-       const { content, important } = request.body
-      
-       Note.findById(request.params.id)
-       .then(note => {
-       if (!note) {
-       return response.status(404).end()
-       }
-       note.content = content
-       note.important = important
-      
-        return note.save().then((updatedNote) => {
-      response.json(updatedNote)
-       })
-      })
-       .catch(error => next(error))
-       })
-      
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+  
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+      note.content = content
+      note.important = important
+     
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
